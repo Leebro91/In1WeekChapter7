ac8ccaee6020b2fa9f2e49f6009db5a201d04136
Leebro91 <67632097+Leebro91@users.noreply.github.com>
2020-09-23 17:51:31 +0900

    Create Q132Cat.java

---
+++ Q132Cat.java
@@ -0,0 +1,16 @@
+class Cat extends Animal {
+  private int age; // 나이
+
+  // 생성자
+  public Cat(String name, int age) {
+    super(name); this.age = age;
+  }
+
+  public void bark() { 
+    System.out.println("야옹!");
+  }
+
+  public String toString() {
+    return age + "살의 " + getName();
+  }
+}
