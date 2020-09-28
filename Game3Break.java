f865f493a6ff3a3a582b54fec9f537f3314c3a4d
Leebro91 <67632097+Leebro91@users.noreply.github.com>
2020-09-28 18:10:16 +0900

    Create Game3Break.java

---
+++ Game3Break.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int i, j;
+    i=10;
+  OUT_WHILE: while(true) {
+    j= 0;
+    while(true) {
+      System.out.print("*");
+      j++;
+    if(j>=i) break;
+    }
+    System.out.println();
+    i--;
+    if(i <= 0) break OUT_WHILE;
+    }
+  }
+}
