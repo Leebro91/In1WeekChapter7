33dee8d28ab8a82471c44e4bb77b4bb7f5d9746e
Leebro91 <67632097+Leebro91@users.noreply.github.com>
2020-09-29 18:03:02 +0900

    Create Game4ReverseString.java

---
+++ Game4ReverseString.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main
+{ 
+	public static void main(String[] args) throws IOException
+	{
+    System.out.println("거꾸로 뒤집을 문자열을 입력해주세요.");
+
+    BufferedReader in= new BufferedReader(new InputStreamReader(System.in));
+    String str= in.readLine();  // 키보드로부터 한 줄 입력 
+
+    StringBuffer sb= new StringBuffer(str);
+    System.out.println(sb.reverse());   // 거꾸로 뒤집은 문자열을 출력
+	}
+}
